51c0db3701fe42c0aa8ccdf92ed1f94c79b62d12
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-06-22 19:19:20 -0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
-class BuscaBinaria:
+class BinarySearch:
 
-    def __init__(self, lista_referencial: list, procura: object, metodo='find'):
+    def __init__(self, procura, referencial:list, metodo:str = 'find'):
         self.opcoes_de_metodos = 'find', 'count', 'locate'
-        self.metodo = BuscaBinaria.if_item_in(metodo, self.opcoes_de_metodos)
-        self.lista_referencial = sorted(lista_referencial) # Ordena a lista, visto que a Busca Binária só será funcional caso a lista referida seja permutada.
+        self.metodo = BinarySearch.if_item_in(metodo, self.opcoes_de_metodos).lower()
+        self.referencial = sorted(referencial) # Ordena a lista, visto que a Busca Binária só será funcional caso a lista referida seja permutada.
         self.procura = procura
 
-
+    
+    
     @staticmethod
     def if_item_in(item, opcoes):
 
@@ -16,18 +17,18 @@ class BuscaBinaria:
 
 
     def fetch(self):
-        left, right = 0, len(self.lista_referencial) - 1 # esqueda e direita
+        INDICE_MIN, INDICE_MAX = 0, len(self.referencial) - 1 # esqueda e direita
         
         if self.metodo == self.opcoes_de_metodos[0]: # find
-            resultado = self.__busca_binaria_find(left, right)
+            resultado = self.__busca_binaria_find(INDICE_MIN, INDICE_MAX)
 
         
         elif self.metodo == self.opcoes_de_metodos[1]: # count
-           resultado = self.__busca_binaria_count(left, right)
+           resultado = self.__busca_binaria_count(INDICE_MIN, INDICE_MAX)
 
 
         elif self.metodo == self.opcoes_de_metodos[2]: # locate
-           resultado = self.__busca_binaria_locate(left, right)
+           resultado = self.__busca_binaria_locate(INDICE_MIN, INDICE_MAX)
 
         return resultado
 
@@ -36,10 +37,10 @@ class BuscaBinaria:
     def __busca_binaria_find(self, l, r):
         while (l <= r):
             meio = (l+r) // 2 # meio
-            if (self.lista_referencial[meio] == self.procura):
+            if (self.referencial[meio] == self.procura):
                 return True
             
-            elif (self.lista_referencial[meio] > self.procura):
+            elif (self.referencial[meio] > self.procura):
                 r = meio-1
 
             else:
@@ -54,23 +55,23 @@ class BuscaBinaria:
         while (l <= r):
             meio = (l+r) // 2 # meio
 
-            if (self.lista_referencial[meio] == self.procura):
+            if (self.referencial[meio] == self.procura):
                 total += 1
                 i = meio+1
 
-                while (i < len(self.lista_referencial)) and self.lista_referencial[i] == self.procura:
+                while (i < len(self.referencial)) and self.referencial[i] == self.procura:
                     total += 1
                     i += 1
 
                 i = meio-1
-                while (i >=  0) and self.lista_referencial[i] == self.procura:
+                while (i >=  0) and self.referencial[i] == self.procura:
                     total += 1
                     i -= 1
 
                 return total
             
 
-            elif (self.lista_referencial[meio] > self.procura):
+            elif (self.referencial[meio] > self.procura):
                 r = meio-1
 
             else:
@@ -85,23 +86,23 @@ class BuscaBinaria:
         while (l <= r):
             meio = (l+r) // 2 # meio
 
-            if (self.lista_referencial[meio] == self.procura):
+            if (self.referencial[meio] == self.procura):
                 lista_auxiliar.append(meio)
                 i = meio+1
 
-                while (i < len(self.lista_referencial)) and self.lista_referencial[i] == self.procura:
+                while (i < len(self.referencial)) and self.referencial[i] == self.procura:
                     lista_auxiliar.append(i)
                     i += 1
 
                 i = meio-1
-                while (i >=  0) and self.lista_referencial[i] == self.procura:
+                while (i >=  0) and self.referencial[i] == self.procura:
                     lista_auxiliar.append(i)
                     i -= 1
 
                 return lista_auxiliar
             
 
-            elif (self.lista_referencial[meio] > self.procura):
+            elif (self.referencial[meio] > self.procura):
                 r = meio-1
 
             else:
@@ -114,9 +115,8 @@ class BuscaBinaria:
 
 if __name__ == '__main__':
     lista = [1,5,5,5,15,17,20,24,67,76]
-
-    app = BuscaBinaria(metodo='find', lista_referencial=lista, procura=5)
+    
+    app = BinarySearch(5, referencial=lista, metodo='find')
     x = app.fetch()
     print(x)
-
 
